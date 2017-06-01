67f98ba7c119abc6dc94af3a66280dab4aef43aa
Zaid Annas <m.zaid.annas@devsinc.com>
2017-06-01 11:17:27 +0500

    Validations updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
     has_many :microposts
+    validates :name, length: { maximum: 50 }, presence: true
+    validates :email, format: { with: /\A([a-z0-9_\.-]+\@[\da-z\.-]+\.[a-z\.]{2,6})\z/i, message: "enter valid email address" }, length: { maximum: 50 }, presence: true
 end
